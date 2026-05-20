0795c2a45c70fdf863165f7bde58295095ed8b12
fayas <mfayas122@gmail.com>
2026-05-20 14:37:46 +0530

    Updated counter component

--- my-react-learning/src/App.jsx
+++ my-react-learning/src/App.jsx
@@ -1,12 +1,10 @@
 import Counter from "./components/counter";
-import ToggleSwitch from "./components/toggle";
 import "./index.css";
 
 export default function App() {
   return (
     <div>
       <Counter />
-      <ToggleSwitch />
     </div>
   );
 }
--- my-react-learning/src/components/counter.jsx
+++ my-react-learning/src/components/counter.jsx
@@ -1,33 +1,25 @@
 import { useState } from "react";
 import "./counter.css";
 
-
 function Counter() {
+    const [count, setcount]= useState(0);
 
-  const [count, setCount] = useState(0);
-
-  function decrease() {
-    setCount((prevCount) => prevCount - 1);
-  }
-
-  function increase() {
-    setCount((prevCount) => prevCount + 1);
-  }
-
-  
+    function increament(){
+        setcount((prevstate)=> prevstate +1)
+    }
+    function decreament(){
+        setcount((prevstate)=> prevstate -1)
+    }
+    function reset(){
+        setcount(0);
+    }  
 
   return (
     <div className="counter">
-      <h1>COUNT: {count}</h1>
-
-      <button onClick={decrease} className="btn">
-        Decrease
-      </button>
-
-      <button onClick={increase} className="btn">
-        Increase
-      </button>
-
+        <h1>{count}</h1>    
+      <button onClick={increament}>Increace</button>
+      <button onClick={decreament}>Decrease</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
